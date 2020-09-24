00859609c056d96603e9e38bb8f1366bf190b61d
hdtk7897 <hdtk7897@gmail.com>
2020-09-24 20:36:56 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/${content.id}`">
+          {{ content.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from "axios";
+  export default {
+    async asyncData() {
+      const { data } = await axios.get(
+        "https://ns-jamstack.microcms.io/api/v1/blog",
+        {
+          headers: { "X-API-KEY": "1542c8bb-3dc3-40a6-b78d-a6aac529f575" }
+        }
+      );
+      return data;
+    }
+  };
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
